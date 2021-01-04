88de0f09994b58af01e678609e3e42635243bc76
harujisaku <harujisaku@gmail.com>
2021-01-04 16:42:36 +0900

    update doc

---
+++ src/harujisaku/text/Util.java
@@ -0,0 +1,49 @@
+package harujisaku.text;
+
+public class Util {
+	private String text;
+	public Util(String text){
+		this.text=text;
+	}
+	/**
+	 * 操作するテキストを設定します.
+	 * @method setText
+	 * @param  text   設定するテキスト
+	 */
+	public void setText(String text){
+		this.text=text;
+	}
+	/**
+	 * 設定しているテキストを取得します.
+	 * @method getText
+	 * @return 設定されているテキスト
+	 */
+	public String getText(){
+		return text;
+	}
+	/**
+	 * 単語の始点を返します.
+	 * @param  index 検索を開始するindex
+	 * @return       単語の始点のindex
+	 */
+	public int indexOfWordFirst(int index){
+		int i=index;
+		while(Character.isLetterOrDigit(text.charAt(i))){
+			i--;
+		}
+		return i;
+	}
+	/**
+	 * 単語の始点を返します.
+	 * @param  text  検索するテキスト
+	 * @param  index 検索を開始するテキスト
+	 * @return       単語の始点のindex
+	 */
+	public static int indexOfWordFirst(String text,int index){
+		int i=index;
+		while(Character.isLetterOrDigit(text.charAt(i))){
+			i--;
+		}
+		return i;
+	}
+}
